bc3013f10059f6e0dfb83e3b97771e23aa383296
ghazwulshaf <ghazwul.s@gmail.com>
2022-12-02 14:08:39 +0800

    update

--- resources/views/landing/layouts/main.blade.php
+++ resources/views/landing/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Coding</title>
-    <link rel="icon" href="{{ asset('assets/img/coding.png') }}" type="icon/png">
+    <link rel="icon" href="{{ asset('assets/img/logo.png') }}" type="icon/png">
 
     {{-- Main CSS --}}
     <link rel="stylesheet" href="{{ asset('css/landing/main.style.css') }}">
