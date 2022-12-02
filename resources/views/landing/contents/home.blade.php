3bcfab3397b71503766145804b4e7eef7c4413ac
ghazwulshaf <ghazwul.s@gmail.com>
2022-12-02 17:10:46 +0800

    update home

--- resources/views/landing/contents/home.blade.php
+++ resources/views/landing/contents/home.blade.php
@@ -4,10 +4,12 @@
     <section id="home">
         <div class="container">
             <div class="content">
-                <h1 class="content__title">Learn to Code</h1>
-                <p class="content__description">Since 2022, Coding serve 20,000 tutorials with the world's largest web developer site</p>
+                <div class="content__text">
+                    <h1 class="content__title">Learn to Code</h1>
+                    <p class="content__description">Since 2022, Coding serve 20,000 tutorials with the world's largest web developer site</p>
+                </div>
                 <img class="content__image" src="{{ asset('assets/img/programming-animate.svg') }}">
-                <a href="#" class="btn w-96">Get Started</a>
+                <a href="#" class="content__button btn">Get Started</a>
             </div>
         </div>
     </section>
