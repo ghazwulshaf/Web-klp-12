b91e1db0e400bd8f4a4cf5937c8e84a735324875
ghazwulshaf <ghazwul.s@gmail.com>
2022-12-03 00:27:37 +0800

    add contents for section right

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -8,7 +8,7 @@
     <link rel="icon" href="{{ asset('assets/img/logo.png') }}" type="icon/png">
 
     {{-- Main CSS --}}
-    <link rel="stylesheet" href="{{ asset('css/landing/main.style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/login/main.style.css') }}">
 
     {{-- Login CSS --}}
     <link rel="stylesheet" href="{{ asset('css/login/login.style.css') }}">
@@ -20,6 +20,34 @@
     <link rel="stylesheet" href="{{ asset('css/components.css') }}">
 </head>
 <body>
-    
+    <div id="section">
+        <section id="section__left" class="section"></section>
+        <section id="section__right" class="section">
+            <div class="content">
+                <div class="content__header">
+                    <img src="{{ asset('assets/img/logo.png') }}" alt="Coding" width="60px">
+                    <h1 class="header__title">Login</h1>
+                </div>
+                <form action="#" class="content__form">
+                    <div class="form-container">
+                        <label for="email" class="form__label">Email Address</label>
+                        <input type="email" name="email" id="email" class="form__input">
+                    </div>
+                    <div class="form-container">
+                        <label for="password" class="form__label">Password</label>
+                        <input type="password" name="password" id="password" class="form__input">
+                        <div class="form-remember">
+                            <input type="checkbox" name="rem-passwd" id="rem-passwd">
+                            <label for="rem-passwd">Remember Password</label>
+                        </div>
+                    </div>
+                    <div class="form-container">
+                        <button type="submit" class="form__submit btn text-light bg-secondary w-full">Login</button>
+                        <span class="form__text">Don't have an account? <a href="#" class="text__sign-up btn-text">Sign up</a></span>
+                    </div>
+                </form>
+            </div>
+        </section>
+    </div>
 </body>
 </html>
