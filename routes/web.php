b954cd264efacac36a31c36d325ef44761b06a5b
ghazwulshaf <ghazwul.s@gmail.com>
2022-12-02 16:50:34 +0800

    add some item for home page

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('landing.contents.home');
+    return view('landing.contents.home', [
+        'title' => 'Home',
+        'style' => 'home',
+    ]);
 });
