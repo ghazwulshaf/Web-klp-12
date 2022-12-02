d47cceaaf7e6dadabb25ede8b3ed24fe8d330bdb
ghazwulshaf <ghazwul.s@gmail.com>
2022-12-02 21:03:19 +0800

    initial login page

---
+++ resources/views/login/login.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Coding | {{ $title }}</title>
+    <link rel="icon" href="{{ asset('assets/img/logo.png') }}" type="icon/png">
+
+    {{-- Main CSS --}}
+    <link rel="stylesheet" href="{{ asset('css/landing/main.style.css') }}">
+
+    {{-- Login CSS --}}
+    <link rel="stylesheet" href="{{ asset('css/login/login.style.css') }}">
+
+    {{-- Utilities CSS --}}
+    <link rel="stylesheet" href="{{ asset('css/utilities.css') }}">
+
+    {{-- Components CSS --}}
+    <link rel="stylesheet" href="{{ asset('css/components.css') }}">
+</head>
+<body>
+    
+</body>
+</html>
