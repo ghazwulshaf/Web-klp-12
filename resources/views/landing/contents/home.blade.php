6e730c411b26a0eb8120aeaa4da67221556002ca
ghazwulshaf <ghazwul.s@gmail.com>
2022-12-02 16:51:57 +0800

    add some style for home page

--- resources/views/landing/contents/home.blade.php
+++ resources/views/landing/contents/home.blade.php
@@ -6,8 +6,8 @@
             <div class="content">
                 <h1 class="content__title">Learn to Code</h1>
                 <p class="content__description">Since 2022, Coding serve 20,000 tutorials with the world's largest web developer site</p>
-                <img class="content__image" src="{{ asset('assets/img/programming-animate.svg') }}" width="360px">
-                <a href="#" class="btn">Get Started</a>
+                <img class="content__image" src="{{ asset('assets/img/programming-animate.svg') }}">
+                <a href="#" class="btn w-96">Get Started</a>
             </div>
         </div>
     </section>
