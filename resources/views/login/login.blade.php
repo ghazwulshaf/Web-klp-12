0216657a0c8f1b02010f3e612e4ce4a2a7d9e505
ghazwulshaf <ghazwul.s@gmail.com>
2022-12-03 15:40:34 +0800

    add more contents in section left

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -25,7 +25,7 @@
 <body>
     <div id="section">
         <section id="section__left" class="section">
-            <div class="content">
+            <div id="content_1" class="content">
                 <div class="content__text">
                     <h1 class="content__label">Designed for Individuals</h1>
                     <p class="content__description">Learn the code and grow your skill, from anywhere</p>
@@ -36,6 +36,30 @@
                     </div>
                 </div>
                 <img class="content__image" src="{{ asset('assets/img/code-typing-pana.svg') }}">
+            </div>
+            <div id="content_2" class="content">
+                <div class="content__text">
+                    <h1 class="content__label">Lot of tutorials</h1>
+                    <p class="content__description">Learn more programming languages with lof of tutorials</p>
+                    <div class="content__pagination">
+                        <span class="page__item"></span>
+                        <span class="page__item active"></span>
+                        <span class="page__item"></span>
+                    </div>
+                </div>
+                <img class="content__image" src="{{ asset('assets/img/open-source-amico.svg') }}">
+            </div>
+            <div id="content_3" class="content show">
+                <div class="content__text">
+                    <h1 class="content__label">Solve problem</h1>
+                    <p class="content__description">Solve your programming's problems with Coding's tutorials</p>
+                    <div class="content__pagination">
+                        <span class="page__item"></span>
+                        <span class="page__item"></span>
+                        <span class="page__item active"></span>
+                    </div>
+                </div>
+                <img class="content__image" src="{{ asset('assets/img/code-typing-rafiki_cropped.svg') }}">
             </div>
         </section>
 
